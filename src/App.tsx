b396154370459b1954bab78d44b7357a2eb04abd
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-07-08 11:21:17 -0300

    Criando Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'; // Temos que importar esse módulo obrigatoriamente se iremos utilizar as rotas da web.
 
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
-}
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
+
+// O Switch é utilizado para que o React verifique rota por rota, se não tiver, ele verificará todas as rotas de uma vez.
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" exact component={Repository} />
+  </Switch>
+);
+
+export default Routes;
