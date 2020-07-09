636f3b67a7584d4a3a4eb8c76c1f5d088aa69ee8
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-07-09 11:50:57 -0300

    Começando a estilizaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'; // Temos que importar esse módulo obrigatoriamente se iremos utilizar as rotas da web.
 
+import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'; /* Importando ícones, neste caso estamos importando o ícone FiChevronRight. */
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './style';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/38424834?s=460&u=30562206e41cd265195a7af9c9d57eb6e6374ea8&v=4"
+            alt="Foto Autor Repositório"
+          />
+          <div>
+            <strong>jonathancmpc/Go-Stack-back-end</strong>
+            <p>Backend Bootcamp</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/38424834?s=460&u=30562206e41cd265195a7af9c9d57eb6e6374ea8&v=4"
+            alt="Foto Autor Repositório"
+          />
+          <div>
+            <strong>jonathancmpc/Go-Stack-back-end</strong>
+            <p>Backend Bootcamp</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/38424834?s=460&u=30562206e41cd265195a7af9c9d57eb6e6374ea8&v=4"
+            alt="Foto Autor Repositório"
+          />
+          <div>
+            <strong>jonathancmpc/Go-Stack-back-end</strong>
+            <p>Backend Bootcamp</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
